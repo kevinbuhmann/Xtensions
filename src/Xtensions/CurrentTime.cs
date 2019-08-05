395386207147b5fd33b821be19c2cb4ea1cfd7db
Kevin Phelps <KevinPhelps11@gmail.com>
2019-08-05 12:13:25 -0500

    feat: add inital current time implementation

---
+++ src/Xtensions/CurrentTime.cs
@@ -0,0 +1,47 @@
+﻿namespace Xtensions
+{
+    using System;
+    using System.Threading;
+
+    /// <summary>
+    /// This class provides a mechanism to mock the current UTC time in unit tests.
+    /// </summary>
+    public sealed class CurrentTime : IDisposable
+    {
+        private static readonly AsyncLocal<DateTime?> MockUtcNow = new AsyncLocal<DateTime?>();
+
+        private CurrentTime(DateTime mockUtcNow)
+        {
+            if (MockUtcNow.Value != null)
+            {
+                throw new InvalidOperationException("Current time is already mocked.");
+            }
+
+            MockUtcNow.Value = mockUtcNow;
+        }
+
+        /// <summary>
+        /// Gets either the current UTC time or the mocked UTC time for the current async context.
+        /// </summary>
+        /// <value>The current UTC time or the mocked UTC time for the current async context.</value>
+        public static DateTime UtcNow => MockUtcNow.Value ?? DateTime.UtcNow;
+
+        /// <summary>
+        /// Sets the mock UTC time to the given value for the current async context.
+        /// </summary>
+        /// <param name="mockUtcNow">The mock UTC time to use for the current async context.</param>
+        /// <returns>An instance of <see cref="CurrentTime" />. Dispose to return the current async context to current time.</returns>
+        public static CurrentTime UseMockUtcNow(DateTime mockUtcNow)
+        {
+            return new CurrentTime(mockUtcNow);
+        }
+
+        /// <summary>
+        /// Returns the current async context to current time.
+        /// </summary>
+        public void Dispose()
+        {
+            MockUtcNow.Value = null;
+        }
+    }
+}
---
+++ tests/Xtensions.Tests/CurrentTimeTests.cs
@@ -0,0 +1,117 @@
+﻿namespace Xtensions.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Xunit;
+
+    public class CurrentTimeTests
+    {
+        [Fact]
+        public void UtcNow_NotMocked_ReturnsCurrentTime()
+        {
+            for (int i = 0; i < 100; ++i)
+            {
+                Assert.InRange(actual: CurrentTime.UtcNow, low: DateTime.UtcNow.AddMilliseconds(-2), high: DateTime.UtcNow);
+            }
+        }
+
+        [Fact]
+        public void UtcNow_Mocked_ReturnsMockTime()
+        {
+            DateTime mockUtcNow = new DateTime(year: 2010, month: 3, day: 17, hour: 18, minute: 23, second: 48);
+
+            using (CurrentTime.UseMockUtcNow(mockUtcNow))
+            {
+                for (int i = 0; i < 100; ++i)
+                {
+                    Assert.Equal(actual: CurrentTime.UtcNow, expected: mockUtcNow);
+                }
+            }
+        }
+
+        [Fact]
+        public async Task UtcNow_Mocked_ReturnsMockTimeInAsyncCode()
+        {
+            DateTime mockUtcNow = new DateTime(year: 2010, month: 3, day: 17, hour: 18, minute: 23, second: 48);
+
+            using (CurrentTime.UseMockUtcNow(mockUtcNow))
+            {
+                for (int i = 0; i < 100; ++i)
+                {
+                    await Task.Delay(1).ConfigureAwait(false);
+                    Assert.Equal(actual: CurrentTime.UtcNow, expected: mockUtcNow);
+                }
+            }
+        }
+
+        [Fact]
+        public void UtcNow_Mocked_SupportsParallelTests()
+        {
+            Parallel.ForEach(Enumerable.Range(1, 25), threadNumber =>
+            {
+                DateTime mockUtcNow = new DateTime(year: 2010, month: 3, day: threadNumber);
+
+                using (CurrentTime.UseMockUtcNow(mockUtcNow))
+                {
+                    for (int i = 0; i < 100; ++i)
+                    {
+                        Assert.Equal(actual: CurrentTime.UtcNow, expected: mockUtcNow);
+                    }
+                }
+            });
+        }
+
+        [Fact]
+        public async Task UtcNow_Mocked_SupportsParallelAsyncTests()
+        {
+            IEnumerable<Task> tasks = Enumerable.Range(1, 10).Select(async threadNumber =>
+            {
+                DateTime mockUtcNow = new DateTime(year: 2010, month: 3, day: threadNumber);
+
+                using (CurrentTime.UseMockUtcNow(mockUtcNow))
+                {
+                    for (int i = 0; i < 100; ++i)
+                    {
+                        await Task.Delay(1).ConfigureAwait(false);
+                        Assert.Equal(actual: CurrentTime.UtcNow, expected: mockUtcNow);
+                    }
+                }
+            });
+
+            await Task.WhenAll(tasks).ConfigureAwait(false);
+        }
+
+        [Fact]
+        public void UtcNow_Mocked_ReturnsCurrentTimeAfterDisposal()
+        {
+            DateTime mockUtcNow = new DateTime(year: 2010, month: 3, day: 17, hour: 18, minute: 23, second: 48);
+
+            using (CurrentTime.UseMockUtcNow(mockUtcNow))
+            {
+                Assert.Equal(actual: CurrentTime.UtcNow, expected: mockUtcNow);
+            }
+
+            Assert.InRange(actual: CurrentTime.UtcNow, low: DateTime.UtcNow.AddMilliseconds(-2), high: DateTime.UtcNow);
+        }
+
+        [Fact]
+        public void UtcNow_MockedTwice_Throws()
+        {
+            DateTime mockUtcNow = new DateTime(year: 2010, month: 3, day: 17, hour: 18, minute: 23, second: 48);
+
+            using (CurrentTime.UseMockUtcNow(mockUtcNow))
+            {
+                InvalidOperationException exception = Assert.Throws<InvalidOperationException>(() =>
+                {
+                    using (CurrentTime.UseMockUtcNow(mockUtcNow))
+                    {
+                    }
+                });
+
+                Assert.Equal(actual: exception.Message, expected: "Current time is already mocked.");
+            }
+        }
+    }
+}
