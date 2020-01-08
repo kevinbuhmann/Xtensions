b925c97ba85af2e3699f773035a2b89e8bef58a6
Kevin Phelps <KevinPhelps11@gmail.com>
2020-01-08 16:39:14 -0600

    chore: remove code from fody test target

--- tests/Xtensions.ArgumentNullGuard.Fody.TestTarget/Program.cs
+++ tests/Xtensions.ArgumentNullGuard.Fody.TestTarget/Program.cs
@@ -1,41 +1,9 @@
 ﻿namespace Xtensions.ArgumentNullGuard.Fody.TestTarget
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Threading.Tasks;
-
     public static class Program
     {
         public static void Main()
         {
-        }
-
-        public static async Task TestAsyncMethod(string value)
-        {
-            Console.WriteLine(value);
-
-            if (value == "a")
-            {
-                while (true)
-                {
-                    await Task.Delay(0);
-                }
-            }
-        }
-
-        public static IEnumerable<string> TestIteratorMethod(string value)
-        {
-            Console.WriteLine(value);
-
-            yield return value;
-
-            if (value == "a")
-            {
-                while (true)
-                {
-                    yield return value;
-                }
-            }
         }
     }
 }
