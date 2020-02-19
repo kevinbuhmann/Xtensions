db58ddc7e5936dd952734ab4fd28d96b87e1b02c
Kevin Phelps <KevinPhelps11@gmail.com>
2020-02-18 23:37:52 -0600

    chore: run tests using x86 and x64 dotnet on Windows

---
+++ .github/workflows/install-dotnet.ps1
@@ -0,0 +1,23 @@
+﻿Param (
+  [Parameter(Mandatory=$true)]
+  [ValidateSet("x86", "x64")]
+  [string] $Architecture
+)
+
+Set-StrictMode -Version latest
+$ErrorActionPreference = "Stop"
+
+. ".\.build\helper-functions.ps1"
+
+$tempDirectory = [io.path]::GetFullPath("$PSScriptRoot\..\..\.tmp")
+$dotnetDirectory = "$tempDirectory\dotnet-$Architecture"
+$dotnetInstallFile = "$tempDirectory\dotnet-install.ps1"
+
+mkdir -Force $tempDirectory > $null
+
+$dotnetInstallUrl = "https://raw.githubusercontent.com/dotnet/cli/master/scripts/obtain/dotnet-install.ps1"
+(New-Object System.Net.WebClient).DownloadFile($dotnetInstallUrl, $dotnetInstallFile)
+
+Safe-Execute-Command { & $dotnetInstallFile -InstallDir $dotnetDirectory -Architecture $Architecture }
+
+Write-Output "::add-path::$dotnetDirectory"
