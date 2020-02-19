88ea69ce66172cf48819ca36a0fdbd143fd327d4
Kevin Phelps <KevinPhelps11@gmail.com>
2020-02-18 21:07:48 -0600

    chore: migrate to simple build script

---
+++ .build/helper-functions.ps1
@@ -0,0 +1,30 @@
+﻿function Execute-Step($stepName, $step) {
+  if ($env:GITHUB_WORKFLOW) {
+    Write-Output "::group:: $stepName"
+  } else {
+    Write-Output ""
+    Write-Output "╬═════════════════════"
+    Write-Output "║ $stepName"
+    Write-Output "╬════════════"
+    Write-Output ""
+  }
+
+  Invoke-Command $step
+
+  if ($env:GITHUB_WORKFLOW) {
+    Write-Output "::endgroup::"
+  }
+}
+
+function Safe-Execute-Command($command) {
+  Invoke-Command $command
+
+  if ($lastExitCode -ne 0) {
+    exit $lastExitCode
+  }
+}
+
+function Invoke-Dotnet($dotnetArgs) {
+  Write-Host "> dotnet $dotnetArgs" -ForegroundColor DarkGray
+  Safe-Execute-Command { Invoke-Expression "dotnet $dotnetArgs" }
+}
